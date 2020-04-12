c9889be2b550ae47146a58f6c95dfeaed56a3147
Enrique Garcia <nitos_san@hotmail.com>
2020-04-12 14:13:32 -0500

    Adding game settings to project without backend

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Map from "./components/Map";
 import GameSettings from "./components/GameSettings"
+import 'materialize-css/dist/css/materialize.css';
+import 'materialize-css/dist/js/materialize.js'
 import "./App.css";
 import {
   nestedArray,
@@ -14,54 +16,79 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      cellsClicked: 1,
       mapObject: {
         rows: 5,
-        cols: 7,
-        bombCount: 12,
+        cols: 5,
+        bombs: 5,
         board: valsAdjacentCounts(
-          populateNestedArray(nestedArray(5, 7), "☀", 12),
+          populateNestedArray(nestedArray(5, 5), "☀", 5),
           "☀"
         ),
-        cellsClicked: 1,
-        cellsState: defaultCellsState(5, 7)
+        cellsState: defaultCellsState(5, 5),
+        visible: false
+      },
+      gameSettings: {
+        user: null,
+        time: 0,
+        gameSelected: 0
       }
     };
   }
 
   incCellsClicked(row, col, flag) {
-    let { rows, cols, bombCount, cellsClicked } = this.state.mapObject;
-    let safeCells = rows * cols - bombCount;
-    this.setState({
-      cellsClicked: cellsClicked + 1
-    });
-    this.updateCellState(row, col, true, flag)
+    let { rows, cols, bombs } = this.state.mapObject;
+    let { cellsClicked } = this.state;
+    let safeCells = rows * cols - bombs;
+    this.setState({ cellsClicked: cellsClicked + 1 });
+    this.updateCellState(row, col, true, flag);
     if (cellsClicked >= safeCells) alert("☀☀☀ You have won! ☀☀☀");
   }
 
   updateCellState(row, col, clicked, flag) {
     let { cellsState } = this.state.mapObject;
-    cellsState[`${row}_${col}`] = { clicked, flag }
-    this.setState({ cellsState })
+    cellsState[`${row}_${col}`] = { clicked, flag };
+    this.setState({ mapObject: { ...this.state.mapObject, cellsState: cellsState}});
+  }
+
+  startGame(rows, cols, bombs) {
+    let mapObject = {
+      rows,
+      cols,
+      bombs,
+      board: valsAdjacentCounts(populateNestedArray(nestedArray(rows, cols), "☀", bombs),"☀"),
+      cellsState: defaultCellsState(rows, cols),
+      visible: true
+    }
+    this.setState({mapObject});
+    this.setState({cellsClicked: 1});
   }
 
   render() {
+    let {mapObject} = this.state
     return (
       <div>
         <nav>
-          <div class="nav-wrapper">
-            <span class="brand-logo center">Minesweeper</span>
+          <div className="nav-wrapper">
+            <span className="brand-logo center">Minesweeper</span>
           </div>
         </nav>
         <div className="container">
           <br />
-          <GameSettings className="center" />
-          <br />
-          <Map
+          <GameSettings 
             className="center"
-            mapObject={this.state.mapObject}
-            incCellsClicked={this.incCellsClicked.bind(this)}
-            updateCellState={this.updateCellState.bind(this)}
+            startGame={this.startGame.bind(this)}
+            gameSettings={this.state.gameSettings}
+          />
+          <br />
+          {mapObject.visible &&
+            <Map
+              className="center"
+              mapObject={this.state.mapObject}
+              incCellsClicked={this.incCellsClicked.bind(this)}
+              updateCellState={this.updateCellState.bind(this)}
             />
+          }
           <br />
         </div>
       </div>
--- react-client/src/components/GameSettings.js
+++ react-client/src/components/GameSettings.js
@@ -1,14 +1,116 @@
 import React, { Component } from "react";
+const { Select, Button, TextInput } = require('react-materialize');
 
 class GameSettings extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      user: null,
+      rows: 5,
+      cols: 5,
+      bombs: 5,
+      time: null,
+      enableUser: true,
+      showExistingUserOptions: false,
+      showGameSettings: false,
+      enableGameSettings: true,
+      showStartGame: false,
+      showGameList: false
+    }
+  }
+
+  selectUser() {
+    this.setState({enableUser: false, showExistingUserOptions: false, showGameSettings: true, showStartGame: true});
+  }
+
+  startGame() {
+    let {rows, cols, bombs} = this.state
+    this.setState({enableGameSettings: false, showStartGame: false});
+    this.props.startGame(rows, cols, bombs);
+  }
+
+  fetchExistingGames() {
+    // TODO
+  }
+
+  loadExistingGame() {
+    // TODO
+  }
+
+  saveGame() {
+    // TODO
   }
 
   render() {
+    const optionValues = [5,6,7,8,9,10,11,12,13,14,15];
+    let { enableUser, showExistingUserOptions, showGameSettings, enableGameSettings, showGameList, showStartGame, user } = this.state
     return (
       <div>
-        ToDo
+        <TextInput 
+          label="Username"
+          value={this.state.user}
+          disabled={!enableUser}
+          onChange={(event) => this.setState({user: event.target.value})}
+        />
+        {enableUser && 
+          <Button onClick={this.selectUser.bind(this)} disabled={user === null || user.length < 1}>Select User</Button>
+        }
+        {showExistingUserOptions &&
+          <div>
+            <Button>Start new Game</Button>
+            <br/><br/>
+            <Button>Load/View Existing Game</Button>
+          </div>
+        }
+        {showGameSettings &&
+          <div className="row">
+            <Select
+              multiple={false}
+              label="Number of Rows"
+              value={this.state.rows}
+              disabled={!enableGameSettings}
+              onChange={(event) => this.setState({rows: event.target.value})}
+            >
+              {optionValues.map(v =>
+                <option value={v}>{v}</option>
+              )}
+            </Select>
+            <Select
+              multiple={false}
+              label="Number of Columns"
+              value={this.state.cols}
+              disabled={!enableGameSettings}
+              onChange={(event) => this.setState({cols: event.target.value})}
+            >
+              {optionValues.map(v =>
+                <option value={v}>{v}</option>
+              )}
+            </Select>
+            <Select
+              multiple={false}
+              label="Number of Bombs"
+              value={this.state.bombs}
+              disabled={!enableGameSettings}
+              onChange={(event) => this.setState({bombs: event.target.value})}
+            >
+              {optionValues.map(v =>
+                <option value={v}>{v}</option>
+              )}
+            </Select>
+          </div>
+        }
+        {showStartGame && <Button onClick={this.startGame.bind(this)}>Start Game</Button> }
+        {showGameList &&
+          <div>
+            Existing games: To Do
+          </div>
+        }
+        {!enableGameSettings &&
+          <div className="row">
+            <p>Time elapsed: 00:00:00</p>
+            <Button>Save and Return to new Settings</Button>
+          </div>
+        }
       </div>
     );
   }
